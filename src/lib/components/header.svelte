ef122f00f8467debd71f4b3038fe67c4999072b1
Pierre Le Gall <pierre@legall.im>
2026-01-19 00:43:19 +0100

    Remove CRT effect

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <header>
-  <div class="crt sticky w-full top-0 z-50 bg-black/96 border-b-1 border-gray-400">
+  <div class="sticky w-full top-0 z-50 bg-black/96 border-b-1 border-gray-400">
     <nav class="align-center flex flex-col sm:justify-between py-2 sm:flex-row sm:space-y-0 mx-auto max-w-3xl px-8">
       <a class="text-primary my-auto mx-auto sm:ml-0" href="/" use:glitch={{ playMode: "click" }}>
         <img
