a9fb61b2669d43c3269bb2fe9440488da18f44dc
Pierre Le Gall <pierre@legall.im>
2026-02-25 16:53:18 +0100

    Fix: home page non-en/fr content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,24 +30,15 @@
 </svelte:head>
 
 <div class="flex min-h-full flex-col justify-center space-y-4 sm:space-y-8">
-
-  {#if language == "en"}
+  {#if language == "fr"}
     <div class="space-y-4 text-center text-lg italic">
-        <p>
-          Once upon a time...
-        </p>
-        <p>
-          Psychedelic storytellers of swarming and soaring tales.
-        </p>
+      <p>Il était une fois...</p>
+      <p>Des conteurs psychédéliques d'histoires grouillantes et planantes.</p>
     </div>
-  {:else if language == "fr"}
+  {:else}
     <div class="space-y-4 text-center text-lg italic">
-        <p>
-          Il était une fois...
-        </p>
-        <p>
-          Des conteurs psychédéliques d'histoires grouillantes et planantes.
-        </p>
+      <p>Once upon a time...</p>
+      <p>Psychedelic storytellers of swarming and soaring tales.</p>
     </div>
   {/if}
 
@@ -69,46 +60,24 @@
     </hgroup>
 
     <div class="mx-auto grid size-fit grid-flow-row grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-    {#each lastReleases as release}
-      <a class="size-fit" href={`/releases/${release.slug}`}>
-        <div class="h-full w-full">
-          <ReleaseCard release={release} />
-        </div>
-      </a>
-    {/each}
+      {#each lastReleases as release}
+        <a class="size-fit" href={`/releases/${release.slug}`}>
+          <div class="h-full w-full">
+            <ReleaseCard release={release} />
+          </div>
+        </a>
+      {/each}
     </div>
   </div>
 
   <div class="space-y-4">
-    {#if language == "en"}
-      <hgroup>
-        <div class="highlight text-center text-4xl bg-black/80 px-2 py-1 w-fit mx-auto">
-          about
-        </div>
-      </hgroup>
-      <p class="text-center">
-        Dripping Tales Records is a collective of friends passionate about the dark and twisted
-        sides of psychedelic music. With a view to promote and share the creations of talented
-        artists, the emphasis is put on deep atmospheres, swarming sounds and textured storytelling
-        without specific restrictions.
-      </p>
-      <p class="text-center">
-        By referring to organic and lively sounds, the vibe we share is strongly communicative and
-        ensures total immersion on the dance floor. The Dripping Tales association revolves around
-        two main pillars: the label and events. While the label focuses on the phonographic
-        production of musical works, the events aim to bring to life this psychedelic, mystical, and
-        dreamlike universe that resonates with each soul.
-      </p>
-      <p class="text-center">
-        Above all, Dripping Tales seeks to be a space of freedom, sharing, and respect, where
-        friendship and kindness are the guiding principles.
-      </p>
-    {:else if language == "fr"}
-      <hgroup>
-        <div class="highlight text-center text-4xl bg-black/80 px-2 py-1 w-fit mx-auto">
-          about
-        </div>
-      </hgroup>
+    <hgroup>
+      <div class="highlight text-center text-4xl bg-black/80 px-2 py-1 w-fit mx-auto">
+        about
+      </div>
+    </hgroup>
+
+    {#if language == "fr"}
       <p class="text-center">
         Dripping Tales Records est un collectif d’amis passionnés par les dimensions sombres et
         tordues de la musique psychédélique. Avec l'objectif de promouvoir et de partager les
@@ -130,8 +99,24 @@
         Avant tout, Dripping Tales se veut un espace de liberté, de partage et de respect, où amitié
         et bienveillance sont maîtres-mots.
       </p>
+    {:else}
+      <p class="text-center">
+        Dripping Tales Records is a collective of friends passionate about the dark and twisted
+        sides of psychedelic music. With a view to promote and share the creations of talented
+        artists, the emphasis is put on deep atmospheres, swarming sounds and textured storytelling
+        without specific restrictions.
+      </p>
+      <p class="text-center">
+        By referring to organic and lively sounds, the vibe we share is strongly communicative and
+        ensures total immersion on the dance floor. The Dripping Tales association revolves around
+        two main pillars: the label and events. While the label focuses on the phonographic
+        production of musical works, the events aim to bring to life this psychedelic, mystical, and
+        dreamlike universe that resonates with each soul.
+      </p>
+      <p class="text-center">
+        Above all, Dripping Tales seeks to be a space of freedom, sharing, and respect, where
+        friendship and kindness are the guiding principles.
+      </p>
     {/if}
   </div>
-
-  
 </div>
