23ba154b793253f95d1fa4f1c89dd8a640c6eacf
theodeam <theo.deamorin@hotmail.fr>
2026-03-21 14:59:36 +0100

    Update home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,15 +32,6 @@
       email="drippingtalesrecords@gmail.com"
     />
   </div>
-    <div class="space-y-4 text-center uppercase">
-      {#if language == "fr"}
-        <p>Il était une fois...</p>
-        <p>Des conteurs psychédéliques d'histoires grouillantes et planantes</p>
-      {:else}
-        <p>Once upon a time...</p>
-        <p>Psychedelic storytellers of swarming and soaring tales</p>
-      {/if}
-    </div>
   <div class="mx-auto space-y-4">
     <hgroup>
       <div class="highlight text-center text-3xl bg-black/80 px-2 py-1 w-fit mx-auto">
@@ -76,43 +67,26 @@
 
     {#if language == "fr"}
       <p class="text-center">
-        Dripping Tales Records est un collectif d’amis passionnés par les dimensions sombres et
-        tordues de la musique psychédélique. Avec l'objectif de promouvoir et de partager les
-        créations d'artistes talentueux, l'accent est mis sur les atmosphères profondes, les
-        sonorités grouillantes et les récits musicaux texturés sans restrictions particulières de
-        style.
+        Dripping Tales Records est un collectif d'amis passionnés par les univers sombres et envoûtants de la trance psychédélique. Sa mission est de partager le travail d'artistes talentueux, en privilégiant une expérience musicale immersive et narrative.
       </p>
       <p class="text-center">
-        En se référant à des sonorités organiques et vivantes, l’énergie transmise se veut fortement
-        communicative et assure une immersion totale sur la piste de danse.
+        Le projet Dripping Tales s'articule autour de deux piliers principaux : le label et les événements.
+        Tandis que le label se concentre sur la production phonographique, les événements offrent un espace psychédélique permettant aux gens de se connecter et de partager des expériences sensorielles et musicales uniques.
       </p>
       <p class="text-center">
-        L’association Dripping Tales évolue autour de deux axes principaux qui sont le label et les
-        événements. Tandis que le label se concentre sur la production phonographique d'œuvres
-        musicales, les événements tendent à rendre vivant cet univers psychédélique, mystique et
-        propice à la rêverie qui fait vibrer l’âme de chacun.
-      </p>
-      <p class="text-center">
-        Avant tout, Dripping Tales se veut un espace de liberté, de partage et de respect, où amitié
-        et bienveillance sont maîtres-mots.
+        Avant tout, Dripping Tales se veut un espace de partage et de respect, où amitié
+        et ouverture les sont maîtres-mots.
       </p>
     {:else}
       <p class="text-center">
-        Dripping Tales Records is a collective of friends passionate about the dark and twisted
-        sides of psychedelic music. With a view to promote and share the creations of talented
-        artists, the emphasis is put on deep atmospheres, swarming sounds and textured storytelling
-        without specific restrictions.
+        Dripping Tales Records is a collective of friends passionate about the dark and twisted realms of psychedelic trance. Its mission is to share the work of talented artists with a focus on immersive and narrative-driven musical experience.
       </p>
       <p class="text-center">
-        By referring to organic and lively sounds, the vibe we share is strongly communicative and
-        ensures total immersion on the dance floor. The Dripping Tales association revolves around
-        two main pillars: the label and events. While the label focuses on the phonographic
-        production of musical works, the events aim to bring to life this psychedelic, mystical, and
-        dreamlike universe that resonates with each soul.
+        The Dripping Tales project revolves around two main pillars: the label and the events. While the label focuses on phonographic production, the events provide a psychedelic platform for people to connect and share unique experiences of music and enjoyment.
       </p>
       <p class="text-center">
-        Above all, Dripping Tales seeks to be a space of freedom, sharing, and respect, where
-        friendship and kindness are the guiding principles.
+        Above all, Dripping Tales seeks to be a space sharing, and respect, where
+        friendship and openness are the guiding principles.
       </p>
     {/if}
   </div>
