e2d729b6bdf5faff685f21804e1921dfc4601f8e
theodeam <theo.deamorin@hotmail.fr>
2026-03-21 15:45:12 +0100

    Translate nav bar

--- src/lib/components/nav-bar.svelte
+++ src/lib/components/nav-bar.svelte
@@ -1,21 +1,42 @@
 <script lang="ts">
   import { page } from "$app/stores"
   import { glitch } from "$lib/actions/glitch"
+  import { getLanguage } from "$lib/languages"
+
+  const language = getLanguage()
 </script>
 
 <div class="select-none">
   <nav class="align-center flex flex-col justify-between sm:py-1 mx-auto max-w-5xl">
     <div class="flex flex-row justify-center space-x-2 sm:space-x-3">
       <a href="/" class={[$page.url.pathname === "/" && "active"]}>
-        <span use:glitch={{ playMode: "click" }}>home</span>
+        <span use:glitch={{ playMode: "click" }}>
+          {#if language == "fr"}
+            accueil
+          {:else}
+            home
+          {/if}
+        </span>
       </a>
       <span>·</span>
       <a href="/artists" class={[$page.url.pathname.startsWith("/artists") && "active"]}>
-        <span use:glitch={{ playMode: "click" }}>artists</span>
+        <span use:glitch={{ playMode: "click" }}>
+          {#if language == "fr"}
+            artistes
+          {:else}
+            artists
+          {/if}
+        </span>
       </a>
       <span>·</span>
       <a href="/music" class={[$page.url.pathname.startsWith("/music") && "active"]}>
-        <span use:glitch={{ playMode: "click" }}>music</span>
+        <span use:glitch={{ playMode: "click" }}>
+          {#if language == "fr"}
+            musique
+          {:else}
+            music
+          {/if}
+        </span>
       </a>
     </div>
   </nav>
