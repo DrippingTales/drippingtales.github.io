00e8ecd765ee84f05b55bc949688458dabe75e55
Pierre Le Gall <pierre@legall.im>
2026-03-26 13:11:35 +0100

    chore: update to new page from $app/state

--- src/lib/components/nav-bar.svelte
+++ src/lib/components/nav-bar.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-  import { page } from "$app/stores"
+  import { page } from "$app/state"
   import { glitch } from "$lib/actions/glitch"
 </script>
 
 <div class="select-none">
   <nav class="align-center flex flex-col justify-between sm:py-1 mx-auto max-w-5xl">
     <div class="flex flex-row justify-center space-x-2 sm:space-x-3">
-      <a href="/" class={[$page.url.pathname === "/" && "active"]}>
+      <a href="/" class={[page.url.pathname === "/" && "active"]}>
         <span use:glitch={{ playMode: "click" }}>
           home
         </span>
       </a>
       <span>·</span>
-      <a href="/artists" class={[$page.url.pathname.startsWith("/artists") && "active"]}>
+      <a href="/artists" class={[page.url.pathname.startsWith("/artists") && "active"]}>
         <span use:glitch={{ playMode: "click" }}>
           artists
         </span>
       </a>
       <span>·</span>
-      <a href="/music" class={[$page.url.pathname.startsWith("/music") && "active"]}>
+      <a href="/music" class={[page.url.pathname.startsWith("/music") && "active"]}>
         <span use:glitch={{ playMode: "click" }}>
           music
         </span>
