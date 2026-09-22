3ed9cf31f0607a5b219f53cf7c9e0f3d01f4ff05
Pierre Le Gall <pierre@legall.im>
2026-09-22 15:11:49 +0200

    chore: remove enhanced-img from vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
-import { enhancedImages } from "@sveltejs/enhanced-img"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
-    enhancedImages(),
     sveltekit(),
     tailwindcss()
   ]
